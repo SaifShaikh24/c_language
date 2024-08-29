67a1985d6759aaf2189ee8f47445b490a05129aa
Saif Shaikh <saifofficial179@gmail.com>
2024-08-29 16:19:26 +0530

    Fifth C_Programme

---
+++ prg5.c
@@ -0,0 +1,15 @@
+//Programme 5
+//Declaration & Initialization
+
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20,c;
+	printf("\n a+b =%d",a+b);
+	printf("\n %d + %d = %d",a,b,a+b);
+	c=a+b;
+	printf("\n %d + %d=%d",a,b,c);
+	return 0;
+}
+
+ 

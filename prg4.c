9b7869bb6ab2e69ed528f8f511096d491954206a
Saif Shaikh <saifofficial179@gmail.com>
2024-08-29 16:16:31 +0530

    Fourth C_Programme

---
+++ prg4.c
@@ -0,0 +1,21 @@
+//Programme 4
+//Comment
+
+#include<stdio.h>
+int main()
+{
+	//printf("\n1Hello");
+	printf("\n\t2Hello");
+	/*printf("\n3Hello");
+	printf("\n4Hello");*/
+	printf("\n5Hello");
+	return 0;
+}
+
+// ---> Single Line Comment
+
+/*
+
+          Multi Line Comment
+
+*/

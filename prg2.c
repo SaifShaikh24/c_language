e08f82e951e138241226371765df8d4cac3dc5e2
Saif Shaikh <saifofficial179@gmail.com>
2024-08-29 16:00:06 +0530

    Commit Second C_programme

---
+++ prg2.c
@@ -0,0 +1,16 @@
+//Programme2
+//Escape Sequence
+
+#include<stdio.h>
+int main()
+{
+	printf("\n1Hello");
+	printf("\n\t2Hello");
+	printf("\n3Hello");
+	printf("\n4Hello");
+	printf("\n5Hello");
+	return 0;
+}
+
+// \n ---> For New Line
+// \t ---> For space or tab

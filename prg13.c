c05dbc139fc069da6c2eb65e25bfe883c51e5a31
Saif Shaikh <saifofficial179@gmail.com>
2024-09-02 16:23:51 +0530

    Sizeof

---
+++ prg13.c
@@ -0,0 +1,12 @@
+//programme 13
+//Sizeof
+
+#include<stdio.h>
+int main()
+{
+	printf("Sizeof of Float %d",sizeof(float));
+	printf("Sizeof of Character %d",sizeof(char));
+	printf("Sizeof of Double %d",sizeof(double));
+	printf("Sizeof of LongDouble %d",sizeof(long double));	
+	return 0;
+}

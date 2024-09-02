de9dab485ae4f91ffa07d42978313a323378a59a
Saif Shaikh <saifofficial179@gmail.com>
2024-09-02 16:26:01 +0530

    Result

---
+++ prg15.c
@@ -0,0 +1,24 @@
+//Programme 15
+//Result
+
+#include<stdio.h>
+int mai()
+{
+	int m,p,c,t,pe;
+	printf("Enter Maths marks");
+	scanf("%d",&m);
+	printf("\nEnter Physics marks");
+	scanf("%d",&p);
+	printf("\nEnter Chemistry marks");
+	scanf("%d",&c);
+	
+	printf("\n Maths marks is=%d",m);	
+	printf("\n Physics marks is=%d",p);
+	printf("\n Chemistry marks is=%d",c);
+		
+	t=m+p+c;
+	printf("\nTotal marks =%d",t);
+	pe=t*100/300;
+	printf("\n Percentage=%d",pe);
+	return 0;
+}

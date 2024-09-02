bee493d6d64a43a12fbd8641d8ae16acf8cd7446
Saif Shaikh <saifofficial179@gmail.com>
2024-09-02 16:24:48 +0530

    Relational Operators

---
+++ prg14.c
@@ -0,0 +1,15 @@
+//programme 14
+//Relational Operators
+
+#include<stdio.h>
+int main()
+{
+	int a=10,b=9;
+	printf("\n 1 a>b=%d",a>b);
+	printf("\n 2 a<b=%d",a<b);
+	printf("\n 3 a>=b=%d",a>=b);
+	printf("\n 4 a<=b=%d",a<=b);
+	printf("\n 5 a==b=%d",a==b);
+	printf("\n 6 a!=b=%d",a!=b);
+	return 0;
+}

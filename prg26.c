66fde85a354cc585dde34a615c9b0868a9283bfa
Saif Shaikh <saifofficial179@gmail.com>
2024-09-18 16:27:50 +0530

    From 3 Numbers Find the largest number

---
+++ prg26.c
@@ -0,0 +1,38 @@
+//From 3 Numbers Find the largest number
+#include<stdio.h>
+int main()
+{
+	 int a,b,c;
+	printf("enter the a:");
+	scanf("%d",&a);
+	printf("enter the b:");
+	scanf("%d",&b);
+	printf("enter the c:");
+	scanf("%d",&c);
+
+	if(a>b)
+	{
+		if(a>c)
+			{
+				printf(" a is largest %d",a);
+			}
+		else
+			{
+				printf(" c is largest %d",c);
+			}
+       }
+	else
+	{
+		if(b>c)
+			{
+				printf(" b is largest %d",b);
+			}
+		else
+			{
+				printf(" c is largest %d",c);
+			}
+	}
+
+
+return 0;
+}

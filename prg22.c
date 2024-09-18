56ca68049cddd379697e6761b7ff5ed37a00f185
Saif Shaikh <saifofficial179@gmail.com>
2024-09-18 16:21:12 +0530

    Number is Negative Or Positive

---
+++ prg22.c
@@ -0,0 +1,24 @@
+//number is positive or negative
+#include<stdio.h>
+
+int main()
+{
+
+	int a;
+
+
+	printf("enter the a");
+	scanf("%d",&a);
+
+
+
+	if(a>0)
+	{
+		printf("a(%d) is positive number",a);
+	}
+	else
+	{
+		printf("a(%d) is negative number",a);
+	}
+	return 0;
+}

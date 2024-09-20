00077de745b0b46c43aaf11931a12cc3678c94d8
Saif Shaikh <saifofficial179@gmail.com>
2024-09-20 16:46:07 +0530

    Print 1 to n

---
+++ prg46.c
@@ -0,0 +1,14 @@
+//Print 1 to n
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("enter the n");
+    scanf(" %d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf(" \n %d",i);
+    }
+    return 0;
+
+}

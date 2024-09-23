c71b7862474e3af3c33335e8d0e822044f759685
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 10:21:06 +0530

    Print 1,4,3,16

---
+++ prg58.c
@@ -0,0 +1,20 @@
+//Print 1,4,3,16
+#include<stdio.h>
+int main()
+{
+    int i,n,multi;
+    printf(" entre the n :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {  multi=i*i;
+            printf("%d",multi);
+        }
+        else
+        {
+            printf("%d",i);
+        }
+    }
+    return 0;
+}

2272417f15ad71e7389f7b0c76e6cd3ca853a4b0
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 10:05:33 +0530

    Print n to 1 with for loop

---
+++ prg52.c
@@ -0,0 +1,15 @@
+//Print n to 1 with for loop
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf(" enter the  n :");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        printf("%d",i);
+
+    }
+    return 0;
+
+}

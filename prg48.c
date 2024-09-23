f55bc3262b64ef10fa47d30d4c6092f3c9a86a81
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 09:52:19 +0530

    Print Odd Numbers

---
+++ prg48.c
@@ -0,0 +1,15 @@
+//Print Odd Numbers
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<=50;i++)
+    {
+        if(i%2==1)
+        {
+            printf(" \n  %d ",i);
+        }
+    }
+    return 0;
+}

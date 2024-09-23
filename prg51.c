180ad3d3bd67ae1371e1fe8c9920bce7777fbd8d
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 10:00:17 +0530

    Print A to Z with their Ascii Value with For Loop

---
+++ prg51.c
@@ -0,0 +1,12 @@
+// Print A to Z with their Ascii Value with For Loop
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    for(i=65;i<=90;i++)
+    {
+        printf(" \n  %d-%c",i,i);
+    }
+    return 0;
+}

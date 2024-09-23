d01eb9eb1f2b9449f0e5bbf9c3cd4f68c857c974
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 10:25:28 +0530

    Print 1,4,12

---
+++ prg60.c
@@ -0,0 +1,15 @@
+//Print 1,4,12
+#include<stdio.h>
+int main()
+{
+    int i=1,j,total,n;
+    printf("enter the n :");
+    scanf("%d",&n);
+
+    for(j=1;j<=n;j*=2)
+    {  total=j*i;
+      printf(" %d",total);
+      i++;
+    }
+    return 0;
+}

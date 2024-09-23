f54345767dce7d90dfa9e05c0cae4f3557aa4c6e
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 09:55:43 +0530

    Print Even Numbers with for Loop

---
+++ prg49.c
@@ -0,0 +1,15 @@
+//Print Even Numbers with for Loop
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<=50;i++)
+    {
+        if(i%2==0)
+        {
+            printf(" \n  %d ",i);
+        }
+    }
+    return 0;
+}

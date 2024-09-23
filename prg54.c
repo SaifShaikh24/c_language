b5869642b3c56031fd057486785797a3b5ebee0b
Saif Shaikh <saifofficial179@gmail.com>
2024-09-23 10:09:52 +0530

    Print 1,3,4...=25

---
+++ prg54.c
@@ -0,0 +1,12 @@
+//Print 1,3,4...=25
+#include<stdio.h>
+int main()
+{
+    int i,total=0;
+    for(i=1;i<=10;i+=2)
+    {
+        total=total+i;
+    }
+    printf("total= %d",total);
+    return 0;
+}

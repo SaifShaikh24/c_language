ec7461ee128debdc95251d239fe78e2185a599d6
Saif Shaikh <saifofficial179@gmail.com>
2024-09-24 08:03:17 +0530

    Star Pattern 4

---
+++ prg75.3.c
@@ -0,0 +1,25 @@
+//Star Pattern
+/*
+         *
+       *   *
+     *   *   *
+   *   *   *   *
+ *   *   *   *   *
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j,s=10;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=s;j++)
+          printf("  ");
+        for(j=1;j<=i;j++)
+        {
+            printf("   *");
+        }
+        s--;
+        printf("\n");
+    }
+    return 0;
+}

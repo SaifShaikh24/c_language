ebee38ea85a7c77de154bf9493fc5c271c3a438d
Saif Shaikh <saifofficial179@gmail.com>
2024-10-07 15:42:41 +0530

    Print Alphabets with their ascii value using do_while

---
+++ prg97.c
@@ -0,0 +1,15 @@
+// Print Alphabets with their ascii value using do_while 65-A ...... 90-Z
+#include<stdio.h>
+int main()
+{
+    int i;
+    i=65;
+    do
+    {
+        printf("%d -%c \t",i,i++);
+
+    }
+    while(i<=90);
+    return 0;
+
+}

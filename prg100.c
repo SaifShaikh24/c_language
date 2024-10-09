26c2258ef81bdfd75ccb0db32056b59e71b8021e
Saif Shaikh <saifofficial179@gmail.com>
2024-10-09 17:20:49 +0530

    Print 1.3.5. total=25 using while

---
+++ prg100.c
@@ -0,0 +1,13 @@
+//Print 1.3.5. total=25 using while
+#include<stdio.h>
+int main()
+{
+    int i,sum=0;
+    i=1;
+    while(i<=10)
+    {   sum=sum+i;
+        i+=2;
+    }
+    printf("%d",sum);
+    return 0;
+}

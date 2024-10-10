c1a35014e5744b802b1fef3dfa8cd76d4c13afd1
Saif Shaikh <saifofficial179@gmail.com>
2024-10-10 16:45:42 +0530

    Print series 1.4.3.16 using while

---
+++ prg104.c
@@ -0,0 +1,24 @@
+//Print series 1.4.3.16 using while
+#include<stdio.h>
+int main()
+{
+    int i,sum=0;
+    i=1;
+    while(i<=10)
+    {
+        if(i%2==0)
+        {
+          sum=i*i;
+          printf("%d \n", sum);
+        }
+        else
+        {
+
+            printf("%d \n",i);
+
+        }
+        i++;
+    }
+    return 0;
+
+}

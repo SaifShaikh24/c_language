be02582825918e26ca1bf325adae955a6d662b05
Saif Shaikh <saifofficial179@gmail.com>
2024-10-10 16:41:45 +0530

    Print Table wih while

---
+++ prg102.c
@@ -0,0 +1,15 @@
+// Print Table wih while
+#include<stdio.h>
+int main()
+{
+    int i,n,total;
+    printf("entre the n:");
+    scanf("%d",&n);
+    i=1;
+    while(i<=10)
+    {
+        printf("%d * %d = %d  \n ",i,n,i*n);
+        i++;
+    }
+    return 0;
+}

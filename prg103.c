cf68ac9bd401367379b0d656cdc9414f70313c2a
Saif Shaikh <saifofficial179@gmail.com>
2024-10-10 16:44:04 +0530

    Print 1.4.9.16.25 with do_while

---
+++ prg103.c
@@ -0,0 +1,16 @@
+// Print 1.4.9.16.25 with do_while
+#include<stdio.h>
+int main()
+{
+    int i,sum=0;
+    i=1;
+    do
+    {
+     sum=i*i;
+     printf("%d ",sum );
+     i++;
+    }
+    while(i<=10);
+    return 0;
+
+}

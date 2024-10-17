d393dd500e2c3b3c4f1d3f288d626305c652a166
Saif Shaikh <saifofficial179@gmail.com>
2024-10-17 17:37:17 +0530

    Print A.C.E... using do while

---
+++ prg107.c
@@ -0,0 +1,14 @@
+// Print A.C.E... using do while
+#include<stdio.h>
+int main()
+{
+    int i;
+    i=63;
+    do
+    {
+        printf("%c \n ",i+=2);
+
+    }
+    while(i<=90);
+    return 0;
+}

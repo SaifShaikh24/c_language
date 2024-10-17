ace92bca71ccc2a5232cf1a0e7c12ce439c413fa
Saif Shaikh <saifofficial179@gmail.com>
2024-10-17 17:36:03 +0530

    Print 1,4,12 using while

---
+++ prg106.c
@@ -0,0 +1,17 @@
+//Print 1,4,12 using while
+
+#include<stdio.h>
+int main()
+{
+    int i,j,sum=0;
+    i=1;
+    j=1;
+    while(i<=15)
+    {
+      sum=j*i;
+      printf("%d \t",sum);
+      j*=2;
+      i++;
+
+    }
+}
